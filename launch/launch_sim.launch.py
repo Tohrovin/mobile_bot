8d88e8497790a0401ba9bbf35c2f3a8cdf85410b
Tohrovin <draon88@gmail.com>
2023-10-23 12:06:26 +0200

    updated launch

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -24,7 +24,18 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    # Get the parameters for gazebo simulation
+    gazebo_params_path = os.path.join(
+                  get_package_share_directory(package_name),'config','gazebo_params.yaml')
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package, as well as pass the parameters from gazebo_params file
+    gazebo = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path }.items()
+         )
+
+    
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
@@ -35,6 +46,8 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
+    
+
     
     diff_drive_spawner = Node(
         package="controller_manager",
@@ -47,6 +60,7 @@ def generate_launch_description():
         executable="spawner",
         arguments=["joint_broad"],
     )
+
 
 
     # Launch them all!
